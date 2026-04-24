7b9189d4dbe16ccb1d2e203701043285f7fd7612
DanyFadhilah <47471393+DanyFadhilah@users.noreply.github.com>
2026-04-24 17:51:50 +0700

    Update SortableHighlightCard.tsx

--- src/components/SortableHighlightCard.tsx
+++ src/components/SortableHighlightCard.tsx
@@ -6,10 +6,9 @@ import HighlightCard from "./HighlightCard";
 type Props = {
   item: Highlight;
   onOpen: () => void;
-  disabled?: boolean;
 };
 
-export default function SortableHighlightCard({ item, onOpen, disabled }: Props) {
+export default function SortableHighlightCard({ item, onOpen }: Props) {
   const {
     attributes,
     listeners,
@@ -17,10 +16,7 @@ export default function SortableHighlightCard({ item, onOpen, disabled }: Props)
     transform,
     transition,
     isDragging,
-  } = useSortable({
-    id: item.id,
-    disabled,
-  });
+  } = useSortable({ id: item.id });
 
   const style = {
     transform: CSS.Transform.toString(transform),
@@ -31,11 +27,16 @@ export default function SortableHighlightCard({ item, onOpen, disabled }: Props)
 
   return (
     <div ref={setNodeRef} style={style} className="h-full min-h-0">
-      <div
-        {...attributes}
-        {...(!disabled ? listeners : {})}
-        className="h-full min-h-0"
-      >
+      <div className="relative h-full min-h-0">
+        
+        <div
+          {...attributes}
+          {...listeners}
+          className="absolute top-2 right-2 z-10 cursor-grab active:cursor-grabbing p-2"
+        >
+          ☰
+        </div>
+
         <HighlightCard item={item} onOpen={onOpen} />
       </div>
     </div>
